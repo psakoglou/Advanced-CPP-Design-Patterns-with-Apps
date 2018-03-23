1e4fad1583df769fa0d4a58f2f991362611d7220
Pavlos Sakoglou <23369146+PavlosSakoglou@users.noreply.github.com>
2018-03-22 20:52:54 -0400

    Fixed typo

--- Singleton Pattern/Singleton.cpp
+++ Singleton Pattern/Singleton.cpp
@@ -2,7 +2,7 @@
 *	© Superharmonic Technologies
 *	Pavlos Sakoglou
 *
-* ====================================================================
+* ===================================================
 *
 *	Singleton Implementation
 *
