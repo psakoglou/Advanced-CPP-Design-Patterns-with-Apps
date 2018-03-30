8e9a797ad4107d9f57652275fcdbbdf5cd388bbb
Pavlos Sakoglou <23369146+PavlosSakoglou@users.noreply.github.com>
2018-03-30 17:58:22 -0400

    Fixed typo

--- Strategy Pattern/SortingStrategy.hpp
+++ Strategy Pattern/SortingStrategy.hpp
@@ -2,7 +2,7 @@
 *	© Superharmonic Technologies
 *	Pavlos Sakoglou
 *
-* ==========================================================
+* ======================================================
 *
 *	SortingStrategy base class definition
 *
